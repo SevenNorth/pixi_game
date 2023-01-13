1fb2f236fcd84bb9b20f3f2e58784bb5803e30b9
冯箭飞 <fengjf@gistack.cn>
2023-01-13 16:38:09 +0800

    tslint

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import './index.less';
 import init from './functions/init';
 const main = (): void => {
   const root = document.getElementById('root');
-  const width = root?.clientWidth, height = root?.clientHeight;
+  const width = root?.clientWidth,
+    height = root?.clientHeight;
   const app = init({
     width,
     height,
     background: '#1099bb',
   });
-  console.log("🚀-fjf : app", app)
-  root?.appendChild(app.view as unknown as Document)
-}
+  console.log('🚀-fjf : app', app);
+  root?.appendChild(app.view as unknown as Document);
+};
 main();
 export default main;
